90d664acb34e8e1f014b9a2042688dfe92ab9b5d
Ξsco <voee178@yahoo.com>
2025-08-22 13:00:05 -0700

    Delete supabase directory

--- supabase/migrations/20250815105843_95c20206-112b-4a3f-9848-3e55e945a7c9.sql
+++
@@ -1,7 +0,0 @@
--- Fix OTP expiry security warning by setting proper auth configuration
--- This sets the OTP expiry to recommended security standards
-UPDATE auth.config 
-SET 
-  otp_exp = 3600,  -- 1 hour expiry (recommended)
-  otp_length = 6   -- 6 digit OTP (standard)
-WHERE TRUE;
